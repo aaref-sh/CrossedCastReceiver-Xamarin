a11e616fac42f5ceb13fec81543825a517c24701
aaref-sh <m.aaref-sh@hotmail.com>
2021-05-20 21:29:17 +0300

    add background image - add toast of copy - style messages - much other

--- CrossedCastReceiver/CrossedCastReceiver.Android/MainActivity.cs
+++ CrossedCastReceiver/CrossedCastReceiver.Android/MainActivity.cs
@@ -1,24 +1,55 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
 using Android.OS;
+using Android.Runtime;
 using Android.Views;
+using Android.Widget;
+using CrossedCastReceiver.Droid;
+using System;
+using Xamarin.Forms;
+[assembly: Xamarin.Forms.Dependency(typeof(MessageAndroid))]
 
 namespace CrossedCastReceiver.Droid
 {
-    [Activity(Label = "CrossedCastReceiver", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ScreenOrientation = ScreenOrientation.Landscape, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+
+    public class MessageAndroid : IMessage
+    {
+        public void LongAlert(string message)
+        {
+            Toast.MakeText(Android.App.Application.Context, message, ToastLength.Long).Show();
+        }
+        public void ShortAlert(string message)
+        {
+            Toast.MakeText(Android.App.Application.Context, message, ToastLength.Short).Show();
+        }
+    }
+    [Activity(Label = "CrossedCastReceiver", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ScreenOrientation = ScreenOrientation.Landscape, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+            MessagingCenter.Subscribe<Object, bool>(this, "Hide", (arg, isHaveBar) => hidenavbar());
             Window.AddFlags(WindowManagerFlags.Fullscreen);
             Window.ClearFlags(WindowManagerFlags.ForceNotFullscreen);
+
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+        }
+
+        [Obsolete]
+        void hidenavbar()
+        {
+            int uiOptions = (int)Window.DecorView.SystemUiVisibility;
+
+            uiOptions |= (int)SystemUiFlags.LowProfile;
+            uiOptions |= (int)SystemUiFlags.Fullscreen;
+            uiOptions |= (int)SystemUiFlags.HideNavigation;
+            uiOptions |= (int)SystemUiFlags.ImmersiveSticky;
+
+            Window.DecorView.SystemUiVisibility = (StatusBarVisibility)uiOptions;
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
